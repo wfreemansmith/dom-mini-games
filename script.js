f4c85e5607ca47bb645965b2ce956b12627fddb6
Wes <wfreemansmith@yahoo.co.uk>
2023-02-27 17:07:43 +0000

    refactor

--- script.js
+++ script.js
@@ -1,108 +1,49 @@
-let playerTurn = "O";
-const totals = { O: 0, X: 0 };
-
 let squares = document.querySelectorAll(".square");
 let reset = document.getElementById("reset");
 let tally = document.getElementById("tally");
-let player = document.getElementById("player")
+let player = document.getElementById("player");
+
+let playerTurn = "O";
+const totals = { O: 0, X: 0 };
 
 function changeButton(square) {
   if (square.innerText === "_") {
     square.innerText = playerTurn;
-  }
 
-  checkWinner();
+    checkWinner();
 
-  if (playerTurn === "O") {
-    playerTurn = "X";
-  } else {
-    playerTurn = "O";
+    playerTurn === "O" ? (playerTurn = "X") : (playerTurn = "O");
+    player.innerText = `Player ${playerTurn}'s turn`;
   }
-  player.innerText = `Player ${playerTurn}'s turn`
 }
 
 checkWinner = () => {
-  //horizontal
-  if (
-    squares[0].innerText === playerTurn &&
-    squares[1].innerText === playerTurn &&
-    squares[2].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
-  }
+  const winners = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  if (
-    squares[3].innerText === playerTurn &&
-    squares[4].innerText === playerTurn &&
-    squares[5].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
-  }
-
-  if (
-    squares[6].innerText === playerTurn &&
-    squares[7].innerText === playerTurn &&
-    squares[8].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
-  }
-
-  //diagonal
-  if (
-    squares[0].innerText === playerTurn &&
-    squares[4].innerText === playerTurn &&
-    squares[8].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
-  }
-
-  if (
-    squares[2].innerText === playerTurn &&
-    squares[4].innerText === playerTurn &&
-    squares[6].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
-  }
-
-  //vertical
-  if (
-    squares[0].innerText === playerTurn &&
-    squares[3].innerText === playerTurn &&
-    squares[6].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
-  }
-
-  if (
-    squares[1].innerText === playerTurn &&
-    squares[4].innerText === playerTurn &&
-    squares[7].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
-  }
-
-  if (
-    squares[2].innerText === playerTurn &&
-    squares[5].innerText === playerTurn &&
-    squares[8].innerText === playerTurn
-  ) {
-    alert(`victory for player ${playerTurn}!!`);
-    totals[playerTurn]++;
-    tally.innerText = `O: ${totals.O} X: ${totals.X}`;
+  
+  for (let i = 0; i < winners.length; i++) {
+    if (
+      squares[winners[i][0]].innerText === playerTurn &&
+      squares[winners[i][1]].innerText === playerTurn &&
+      squares[winners[i][2]].innerText === playerTurn
+      // iterates throught the above array, checking if there are any winning patterns
+      
+      ) {
+      alert(`Victory for player ${playerTurn}!!`);
+      totals[playerTurn]++;
+      tally.innerText = `O: ${totals.O} X: ${totals.X}`;
+      // if there are winning patterns: alert, add one to total, update totals
+    
+    }
   }
 };
 
@@ -121,11 +62,3 @@ squares.forEach((square) => {
 reset.addEventListener("click", () => {
   resetButton();
 });
-
-// function changePet() {
-//   if (paragraph.innerText === "catEmoji") {
-//     pet.innerText = "dog";
-//   } else if (pet.innerText === "dogEmoji") {
-//     pet.innerText = "cat";
-//   }
-// }
