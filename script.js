c398746138f44a890ad89a5142cdfcb2081d6925
Wes <wfreemansmith@yahoo.co.uk>
2023-02-27 16:19:48 +0000

    naughts and crosses functionality sorted

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let playerTurn = "o";
+const totals = { o: 0, x: 0 };
 
 let squares = document.querySelectorAll(".square");
 let reset = document.getElementById("reset");
+let tally = document.getElementById("tally");
+let player = document.getElementById("player")
 
 function changeButton(square) {
-  console.log(squares);
-
   if (square.innerText === "_") {
     square.innerText = playerTurn;
   }
@@ -17,54 +18,108 @@ function changeButton(square) {
   } else {
     playerTurn = "o";
   }
-
-
-  //   if square = empty string, then square = player turn
-  //   if playerTurn = o, let it be x
+  player.innerText = `Player ${playerTurn}'s turn`
 }
 
 checkWinner = () => {
-    //horizontal
-    if (squares[0].innerText === playerTurn && squares[1].innerText === playerTurn && squares[2].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}        location.reload();
-        !!`);
-      }   
-      
-      if (squares[3].innerText === playerTurn && squares[4].innerText === playerTurn && squares[5].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}!!`);
-      }
+  //horizontal
+  if (
+    squares[0].innerText === playerTurn &&
+    squares[1].innerText === playerTurn &&
+    squares[2].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
 
-      if (squares[6].innerText === playerTurn && squares[7].innerText === playerTurn && squares[8].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}!!`);
-      }
+  if (
+    squares[3].innerText === playerTurn &&
+    squares[4].innerText === playerTurn &&
+    squares[5].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
 
-      //diagonal
-      if (squares[0].innerText === playerTurn && squares[4].innerText === playerTurn && squares[8].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}!!`);
-      }   
-      
-      if (squares[2].innerText === playerTurn && squares[4].innerText === playerTurn && squares[6].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}!!`);
-      }
+  if (
+    squares[6].innerText === playerTurn &&
+    squares[7].innerText === playerTurn &&
+    squares[8].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
 
-      //vertical
-      if (squares[0].innerText === playerTurn && squares[3].innerText === playerTurn && squares[6].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}!!`);
-      }   
-      
-      if (squares[1].innerText === playerTurn && squares[4].innerText === playerTurn && squares[7].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}!!`);
-      }
+  //diagonal
+  if (
+    squares[0].innerText === playerTurn &&
+    squares[4].innerText === playerTurn &&
+    squares[8].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
 
-      if (squares[2].innerText === playerTurn && squares[5].innerText === playerTurn && squares[8].innerText === playerTurn) {
-        alert(`victory for player ${playerTurn}!!`);
-      }
+  if (
+    squares[2].innerText === playerTurn &&
+    squares[4].innerText === playerTurn &&
+    squares[6].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
+
+  //vertical
+  if (
+    squares[0].innerText === playerTurn &&
+    squares[3].innerText === playerTurn &&
+    squares[6].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
+
+  if (
+    squares[1].innerText === playerTurn &&
+    squares[4].innerText === playerTurn &&
+    squares[7].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
+
+  if (
+    squares[2].innerText === playerTurn &&
+    squares[5].innerText === playerTurn &&
+    squares[8].innerText === playerTurn
+  ) {
+    alert(`victory for player ${playerTurn}!!`);
+    totals[playerTurn]++;
+    tally.innerText = `O: ${totals.o} X: ${totals.x}`;
+  }
+};
+
+function resetButton() {
+  squares.forEach((square) => {
+    square.innerText = "_";
+  });
 }
 
 squares.forEach((square) => {
   square.addEventListener("click", () => {
     changeButton(square);
   });
+});
+
+reset.addEventListener("click", () => {
+  resetButton();
 });
 
 // function changePet() {
